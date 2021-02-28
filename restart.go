459a5fbdcb1b1ec241db3e4904b2e7b69b76aa67
biter777 <33280398+biter777@users.noreply.github.com>
2021-02-28 16:54:09 +0300

    Add files via upload

--- restart.go
+++ restart.go
@@ -18,28 +18,28 @@ var (
 	DebugMode bool
 	cmd       *exec.Cmd // cmd - command being prepared for run
 	start     chan int  // start - semaphore for start cmdStart() with exit code
-	id        string    // id - unique ID (or name) for app
+	appid     string    // id - unique ID (or name) for app
 )
 
 func init() {
 	Logger = &defaultLogger{}
-	
+
 	exe, err := os.Executable()
 	if err != nil {
 		panic(err)
 	}
-	id = getID()
+	appid = getID()
 	cmd = exec.Command(exe, os.Args[1:]...)
 	start = make(chan int, 1)
 	go cmdStart()
 }
 
 func getID() string {
-	id := flag.String("id", "", "unique ID (or name) for app")
+	id := flag.String("appid", "", "unique ID (or name) for app")
 	flag.Parse()
 	if id == nil || *id == "" {
 		*id = generateID()
-		os.Args = append(os.Args, "-id", *id)
+		os.Args = append(os.Args, "-appid", *id)
 	}
 	if DebugMode {
 		Logger.Printf("restart::getID: app id: %v", *id)
@@ -72,5 +72,5 @@ func Now(exitCode int, err error) {
 
 // ID - return a unique id of crawler's manager, which used for setup job for monitorus server
 func ID() string {
-	return id
+	return appid
 }
